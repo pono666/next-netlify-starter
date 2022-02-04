a9769a538d0a8d53d2e9d7ba962354367fbe5b3b
Anthony Ponomarenko <aponomarenko@squiz.net>
2022-02-04 19:30:05 +1000

    done

--- pages/index.js
+++ pages/index.js
@@ -10,15 +10,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <Header title="DENG" />
-        <p className="description">
-    <img src="https://lh3.googleusercontent.com/RnMqPUyUCDy8hus5ElvqU5jOjGWEuw_q4t2LjRKJUEB-6K_YAYgRH_X9bWPRbCGEdSm9t2gafh_9xDNlBhvdOC6n-Y9Jj5HC0JysUWU=w600" />
-          <a href="https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963/46070014432019865234053906931457523796796472796958872296696882100657713053697">get the nft</a>
-        </p>
-      </main>
+      <img class="dengimage" src="https://lh3.googleusercontent.com/RnMqPUyUCDy8hus5ElvqU5jOjGWEuw_q4t2LjRKJUEB-6K_YAYgRH_X9bWPRbCGEdSm9t2gafh_9xDNlBhvdOC6n-Y9Jj5HC0JysUWU=w600" />
 
-      <Footer />
+    
+          <a class="denglink" href="https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963/46070014432019865234053906931457523796796472796958872296696882100657713053697">&lt;get the nft&gt;</a>
+        
+
     </div>
   )
 }

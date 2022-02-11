02093e3a2080750f8f55e575397d967e03f9ad49
Anthony Ponomarenko <aponomarenko@squiz.net>
2022-02-11 15:47:39 +1000

    removed consoles

--- public/main.js
+++ public/main.js
@@ -3,37 +3,25 @@ window.onload = function() {
     const originalAspect = scroller.width / scroller.height;
 
     const setPosition = (animate=false) =>{
-        //window.onscroll = null;
 
-        //let aspectratio = 
-
-        //const scroller = document.querySelector('.dengimage');
         let windowheight = window.innerHeight;
         let height = scroller.width ;
         
 
         let windowwidth = window.innerWidth ;
-        let width = scroller.width ;
-        
-
-
-       // let aspectratio = width / height;
+        let width = scroller.width ;        
 
         width = windowwidth;
         height = windowheight;
         let testHeight = windowwidth / originalAspect;
-        console.log('testHeight', testHeight, windowheight)
        
         if(testHeight < windowheight){
             height = windowheight;
             width = height * originalAspect;
-            console.log('setHeight', height)
        }else{
             height = testHeight;
             width = windowwidth;
-            console.log('!!setHeight', height)
        }
-       console.log(width,height )
 
 
         scroller.width = width;
@@ -44,16 +32,12 @@ window.onload = function() {
         let hts =  (height -  windowheight) / 2 ;
         let wts =  (width -  windowwidth) / 2;
         
-       // if(animate === true){
             document.querySelector('body').scrollTo({
                 top: hts,
                 left: wts,
                 behavior: 'smooth'
               })
-        //}else{
-        //    document.querySelector('body').scrollTo(wts, hts);
-       // }
-      //  window.onscroll = resizeme;
+        
 
     }    
 
